ca57c0e7627d1b94bd86f030222611aab63a5394
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2021-11-04 18:07:47 +0100

    fix: py:percent format

--- notebooks/03-Egalités structurelles et référentielles.py
+++ notebooks/03-Egalités structurelles et référentielles.py
@@ -3,8 +3,8 @@
 #   jupytext:
 #     text_representation:
 #       extension: .py
-#       format_name: light
-#       format_version: '1.5'
+#       format_name: percent
+#       format_version: '1.3'
 #       jupytext_version: 1.13.0
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
@@ -12,42 +12,61 @@
 #     name: python3
 # ---
 
+# %%
 r = l = ["bold"]
 
+# %%
 l.append("large")
 
+# %%
 r is l
 
+# %%
 r == l
 
+# %%
 r
 
+# %%
 l
 
+# %%
 id(r)
 
+# %%
 id(l)
 
+# %%
 id(l) == id(r) # equiv. to l is r.
 
+# %% [markdown]
 # -------
 
+# %%
 r = l = ["bold"]
 
+# %%
 l = l + ["large"]
 
+# %%
 r is l 
 
+# %%
 r == l
 
+# %% [markdown]
 # -----
 
+# %%
 l = ["large"]
 
+# %%
 r = ["large"]
 
+# %%
 r is l
 
+# %%
 r == l
 
-
+# %%
