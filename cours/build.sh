c12c9d1a9fb349801ca96014892ae0ddec69fbd4
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2021-12-31 14:46:50 +0100

    added anchors

--- cours/build.sh
+++ cours/build.sh
@@ -5,6 +5,7 @@ pandoc --standalone --toc \
   --no-highlight \
   --include-in-header html/fonts.html \
   --include-in-header html/plan.html \
+  --include-in-header html/links.html \
   -V lang=fr \
   --mathjax \
   -o index.html index.md
